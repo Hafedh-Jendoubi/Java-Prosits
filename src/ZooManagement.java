f23ee95ea36646a5fcc959a13a281c2142e5345d
Hafedh-Jendoubi <hafedh.jendoubi@esprit.tn>
2023-09-27 10:58:56 +0100

    Prosit 3 done

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -4,7 +4,7 @@ public class ZooManagement {
     public static void main(String[] args){
         //-------------------------------- Prosit 1 -------------------------------------//
 
-        int nbrCages = 20;
+        /*int nbrCages = 20;
         String zooName = "my zoo";
 
         System.out.println(zooName + " comporte " + nbrCages + " cages");
@@ -17,14 +17,17 @@ public class ZooManagement {
         System.out.println("Enterer le nom du zoo: ");
         zooName = scn1.nextLine();
 
-        System.out.println(zooName + " comporte " + nbrCages + " cages");
+        System.out.println(zooName + " comporte " + nbrCages + " cages");*/
 
 
         //-------------------------------- Prosit 2 -------------------------------------//
 
-        Zoo myZoo = new Zoo("myZoo", "Tunis", 3);
+        Zoo myZoo = new Zoo("myZoo", "Tunis");
+        Zoo myZoo2 = new Zoo("myZoo2", "Ben Gerdan");
+        Zoo result;
         Animal a1 = new Animal("Otariidae", "Lion", 10, true);
         Animal a2 = new Animal("Canidae", "Wolf", 8, false);
+        Animal a3 = new Animal("Canidae", "Wolf", 20, false);
         Animal a4 = new Animal("Otariidae", "Lion", 10, true); //identique de a1 pour vérifier addAnimal() function and searchAnimal()
 
         //myZoo.displayZoo();
@@ -32,10 +35,14 @@ public class ZooManagement {
 
         myZoo.addAnimal(a1);
         myZoo.addAnimal(a2);
+        myZoo.addAnimal(a3);
         System.out.println(myZoo.addAnimal(a4));
         myZoo.displayZoo();
 
         myZoo.removeAnimal(a1);
         myZoo.displayZoo();
+        System.out.println(myZoo.isZooFull()); //False because myZoo ain't full.
+        result = myZoo.comparerZoo(myZoo, myZoo2);
+        System.out.println("La résultat de comparerZoo() est: " + result.name);
     }
 }
